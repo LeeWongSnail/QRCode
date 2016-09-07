60b5a8bc0f0920df49a8cb6ea55eb1ef9ee71ccb
LeeWongSnail <@wangli_0632@163.com>
2016-09-07 17:01:41 +0800

    添加注释

--- QRCode/QRImage/QRService.h
+++ QRCode/QRImage/QRService.h
@@ -14,14 +14,32 @@
 
 + (instancetype)shared;
 
-//生成二维码的方法、
-
+/**
+ *  生成二维码图片的方法
+ *
+ *  @param aQRSourceString 生成的二维码包含的图片
+ *  @param imageSize       生成图片的尺寸
+ *  @param aCompletion     生成完成的回调
+ */
 - (void)createQRImage:(NSString *)aQRSourceString size:(CGFloat)imageSize completion:(void (^)(UIImage *result))aCompletion;
 
-//扫描二维码的方法
+/**
+ *  扫描二维码的方法
+ *
+ *  @param windowSize 显示位置
+ *  @param viewSize   可以传递屏幕尺寸
+ *  @param scanResult 扫描结果的字符串
+ *
+ *  @return 返回扫描的layer 需要使用者添加到需要显示扫描效果的view上
+ */
 - (AVCaptureVideoPreviewLayer *)scanQRImage:(CGRect)windowSize viewSize:(CGRect)viewSize result:(void (^)(NSString *aQRCode))scanResult;
 
-//图片中识别二维码的方法
+/**
+ *  识别图中的二维码
+ *
+ *  @param aImage      图片
+ *  @param aCompletion 识别完成的回调
+ */
 - (void)recognitionQRCodeFromImage:(UIImageView *)aImage completion:(void (^)(NSString *result,NSError *error))aCompletion;
 
 @end
--- QRCode/QRImage/UIImage+QRCode.h
+++ QRCode/QRImage/UIImage+QRCode.h
@@ -19,9 +19,40 @@ typedef enum {
 }QRPositionType;
 
 @interface UIImage (QRCode)
-/**之前的方法 */
+
+/**
+ *  生成二维码图片的方法
+ *
+ *  @param string 二维码包含的内容
+ *  @param size   图片的尺寸
+ *  @param topimg 如果中间需要添加 头像可以传入此参数
+ *
+ *  @return 包含要生成内容的二维码图片
+ */
 + (UIImage *)qrImageForString:(NSString *)string imageSize:(CGFloat)size Topimg:(UIImage *)topimg;
+
+/**
+ *  生成二维码图片的方法
+ *
+ *  @param string       二维码包含的内容
+ *  @param size         图片的尺寸
+ *  @param pointType    点的类型（QRPointRect，QRPointRound）
+ *  @param positionType 位置的类型 （QRPositionNormal，QRPositionRound）
+ *  @param color        背景颜色
+ *
+ *  @return 包含要生成内容的二维码图片
+ */
 +(UIImage *)qrImageForString:(NSString *)string imageSize:(CGFloat)size withPointType:(QRPointType)pointType withPositionType:(QRPositionType)positionType withColor:(UIColor *)color;
-/**自己添加 */
+
+/**
+ *  生成二维码图片的方法
+ *
+ *  @param string 二维码包含的内容
+ *  @param size    图片的尺寸
+ *  @param topimg 如果中间需要添加 头像可以传入此参数
+ *  @param color  背景颜色
+ *
+ *  @return 包含要生成内容的二维码图片
+ */
 +(UIImage*)qrImageForString:(NSString *)string imageSize:(CGFloat)size Topimg:(UIImage *)topimg withColor:(UIColor*)color;
 @end
